633ff5b51fa2c12f2fe9e709e1df310853e233ae
MKashanButt <kashanbutt566@gmail.com>
2025-07-23 20:15:25 +0000

    add project

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,117 @@
+"use client"
+
+import * as React from "react"
+import {
+  AudioWaveform,
+  BookOpen,
+  Bot,
+  Command,
+  Frame,
+  GalleryVerticalEnd,
+  Map,
+  PieChart,
+  SquareTerminal,
+  Database,
+} from "lucide-react"
+
+import { NavMain } from "@/components/nav-main"
+import { NavProjects } from "@/components/nav-projects"
+import { NavUser } from "@/components/nav-user"
+import { TeamSwitcher } from "@/components/team-switcher"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+
+const data = {
+  user: {
+    name: "Admin",
+    email: "admin@squad.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  teams: [
+    {
+      name: "Acme Inc",
+      logo: GalleryVerticalEnd,
+      plan: "Enterprise",
+    },
+    {
+      name: "Acme Corp.",
+      logo: AudioWaveform,
+      plan: "Startup",
+    },
+    {
+      name: "Evil Corp.",
+      logo: Command,
+      plan: "Free",
+    },
+  ],
+  navMain: [
+    {
+      title: "Squad Medical Supplies",
+      url: "#",
+      icon: Database,
+      isActive: true,
+      items: [
+        {
+          title: "Inquiries",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Reshape Medical Equipments",
+      url: "#",
+      icon: Database,
+      items: [
+        {
+          title: "Inquiries",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "Bargain Medical Supply",
+      url: "#",
+      icon: Database,
+      items: [
+        {
+          title: "Inquiries",
+          url: "#",
+        },
+      ],
+    },
+    {
+      title: "S8 Medical Equipment",
+      url: "#",
+      icon: Database,
+      items: [
+        {
+          title: "Inquiries",
+          url: "#",
+        },
+      ],
+    },
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar variant="floating" collapsible="icon" {...props}>
+      <SidebarHeader>
+        <TeamSwitcher teams={data.teams} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+        {/* <NavProjects projects={data.projects} /> */}
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
